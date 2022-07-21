f531eae749c59f7d4d545b406407ee56fa7458f8
Evgeny Baryshnikov <svdome@yandex.ru>
2022-07-21 08:45:58 +0300

    .

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ajax example</title>
+    <script type="text/javascript" src="script.js"></script>
 </head>
 <body>
     <input type="text" id="usertext" oninput="Process()">
     <input type="submit">
     <div id="result"></div>
+
+    
 </body>
 </html>
 
